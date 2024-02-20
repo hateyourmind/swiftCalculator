7eff57fe23d373f5299390be5794de0dcadded2e
hateyourmind <hateyourmind@yandex.ru>
2024-02-20 16:44:33 +0300

    Refactoring #1

--- simpleCalculator/simpleCalculator/main.swift
+++ simpleCalculator/simpleCalculator/main.swift
@@ -1,34 +1,45 @@
+func getDataFromUser(message: String) -> String {
+    print(message)
+    return readLine() ?? ""
+}
+
+func showResult(_ result: Int) {
+    let result = String(result)
+    let message = "Результат:"
+    print(message + " " + result)
+}
+
 print("Добро пожаловать в простой калькулятор!")
 
-print("Выберите операцию: +, -, * или /")
-let operation = readLine() ?? ""
-
-print("Введите первое целое число:")
-let numberOne = readLine() ?? ""
-
-print("Введите второе целое число:")
-let numberTwo = readLine() ?? ""
+let operation = getDataFromUser(message: "Выберите операцию: +, -, * или /")
+let numberOne = getDataFromUser(message: "Введите первое целое число:")
+let numberTwo = getDataFromUser(message: "Введите второе целое число:")
 
 print("Вычисляю пример: \(numberOne) \(operation) \(numberTwo)")
 
 if let numberOne = Int(numberOne) {
     if let numberTwo = Int(numberTwo) {
-        switch operation {
-        case "+": print("Результат: \(numberOne + numberTwo)")
-        case "-": print("Результат: \(numberOne - numberTwo)")
-        case "*": print("Результат: \(numberOne * numberTwo)")
-        case "/":
-            if numberTwo != 0 {
-                print("Результат: \(numberOne / numberTwo)")
-            } else {
-                print("На ноль делить нельзя!")
-            }
-                
-        default: print("Вы выбрали неверную операцию!")
-        }
+        calculate(operation: operation, numberOne: numberOne, numberTwo: numberTwo)
     } else {
         print("Вы ввели неверное второе число!")
     }
 } else {
     print("Вы ввели неверное первое число!")
+}
+
+
+func calculate(operation: String, numberOne: Int, numberTwo: Int) {
+    switch operation {
+    case "+": showResult(numberOne + numberTwo)
+    case "-": showResult(numberOne - numberTwo)
+    case "*": showResult(numberOne * numberTwo)
+    case "/":
+        if numberTwo != 0 {
+            showResult(numberOne / numberTwo)
+        } else {
+            print("На ноль делить нельзя!")
+        }
+            
+    default: print("Вы выбрали неверную операцию!")
+    }
 }
