fb1b18192494bd2c7d6c7884b0ca984a81cc8540
hateyourmind <hateyourmind@yandex.ru>
2024-02-26 16:44:29 +0300

    Refactoring #2

--- simpleCalculator/simpleCalculator/main.swift
+++ simpleCalculator/simpleCalculator/main.swift
@@ -1,45 +1,66 @@
+import Darwin
+
 func getDataFromUser(message: String) -> String {
     print(message)
     return readLine() ?? ""
 }
 
 func showResult(_ result: Int) {
-    let result = String(result)
-    let message = "Результат:"
-    print(message + " " + result)
+    print("Результат: \(String(result))")
 }
 
 print("Добро пожаловать в простой калькулятор!")
 
-let operation = getDataFromUser(message: "Выберите операцию: +, -, * или /")
-let numberOne = getDataFromUser(message: "Введите первое целое число:")
-let numberTwo = getDataFromUser(message: "Введите второе целое число:")
+var history: [String] = []
 
-print("Вычисляю пример: \(numberOne) \(operation) \(numberTwo)")
-
-if let numberOne = Int(numberOne) {
-    if let numberTwo = Int(numberTwo) {
-        calculate(operation: operation, numberOne: numberOne, numberTwo: numberTwo)
-    } else {
-        print("Вы ввели неверное второе число!")
+while true {
+    let operation = getDataFromUser(message: "Выберите операцию: +, -, * или /. Для завершения работы введите q. Для просмотра истории вычислений введите h.")
+    if operation == "q" {
+        exit(0)
+    } else if operation == "h" {
+        for example in history {
+            print(example)
+        }
+        continue
     }
-} else {
-    print("Вы ввели неверное первое число!")
+    
+    let numberOne = getDataFromUser(message: "Введите первое целое число:")
+    let numberTwo = getDataFromUser(message: "Введите второе целое число:")
+    let example = "\(numberOne) \(operation) \(numberTwo)"
+    print("Вычисляю пример: \(example)")
+
+    if let numberOne = Int(numberOne) {
+        if let numberTwo = Int(numberTwo) {
+            let result = calculate(operation: operation, numberOne: numberOne, numberTwo: numberTwo)
+            if let result = result {
+                showResult(result)
+                history.append(example + " = " + String(result))
+            }
+        } else {
+            print("Вы ввели неверное второе число!")
+        }
+    } else {
+        print("Вы ввели неверное первое число!")
+    }
+    print("")
+    print("————————————————————————————————")
+    print("")
 }
 
-
-func calculate(operation: String, numberOne: Int, numberTwo: Int) {
+func calculate(operation: String, numberOne: Int, numberTwo: Int) -> Int? {
     switch operation {
-    case "+": showResult(numberOne + numberTwo)
-    case "-": showResult(numberOne - numberTwo)
-    case "*": showResult(numberOne * numberTwo)
+    case "+": return numberOne + numberTwo
+    case "-": return numberOne - numberTwo
+    case "*": return numberOne * numberTwo
     case "/":
         if numberTwo != 0 {
-            showResult(numberOne / numberTwo)
+            return numberOne / numberTwo
         } else {
             print("На ноль делить нельзя!")
+            return nil
         }
-            
-    default: print("Вы выбрали неверную операцию!")
+    default: 
+        print("Вы выбрали неверную операцию!")
+        return nil
     }
 }
